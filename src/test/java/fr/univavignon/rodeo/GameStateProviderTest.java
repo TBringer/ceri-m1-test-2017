dc7b403ab9d27f02c28fab67f42a36deaeecb8a1
TBringer <nobuta.tb@gmail.com>
2018-05-25 23:32:31 +0200

    Fixing bugs in last tests

--- src/test/java/fr/univavignon/rodeo/GameStateProviderTest.java
+++ src/test/java/fr/univavignon/rodeo/GameStateProviderTest.java
@@ -10,15 +10,15 @@ import fr.univavignon.rodeo.imp.GameStateProvider;
 public class GameStateProviderTest extends IGameStateProviderTest {
 	
 	
-	@Test 
+	@Test
 	public void testGetNull(){
 		final IGameStateProvider gameSP = new GameStateProvider();
-		gameSP.get(null);
+		assertEquals(gameSP.get(null), null);
 	}
 	
 	@Test 
 	public void testGet(){
 		final IGameStateProvider gameSP = new GameStateProvider();
-		assertEquals(gameSP.get("reprise"), null);
+		assertEquals(gameSP.get(null), null);
 	}
 }
